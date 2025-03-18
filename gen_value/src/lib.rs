be77401d74caec2dbfc3af576a063427b4c9a23d
Bryant Luk <code@bryantluk.com>
2025-03-18 12:17:24 -0500

    style: fix qualifications

--- gen_value/src/lib.rs
+++ gen_value/src/lib.rs
@@ -117,7 +117,7 @@ pub struct Error {
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> core::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Display::fmt(&self.code, f)
     }
 }
